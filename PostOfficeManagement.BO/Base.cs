09b09b99084eb3826f4f58e3afd7bdc1687c25f1
KingBesfort <105106291+KingBesfort@users.noreply.github.com>
2022-05-24 02:58:50 +0200

    Add files via upload

---
+++ PostOfficeManagement.BO/Base.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostOfficeManagement.BO
+{
+    public class Base
+    {
+        public int Id { get; set; }
+        public int InsertBy { get; set; }
+        public DateTime InsertDate { get; set; }
+        public int LUN { get; set; }
+        public int LUB { get; set; }
+        public DateTime LUD { get; set; }
+    }
+}
